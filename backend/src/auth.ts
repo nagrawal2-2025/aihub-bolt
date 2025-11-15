0a7c687a6eb271a605884b0ce871faf7ab48a94f
nagrawal2-2025 <nagrawal2@deloitte.com>
2025-11-15 16:37:49 +0530

    Updated package.json

--- backend/src/auth.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { loginUser } from '../controllers/authController';
-
-const router = express.Router();
-
-// Login-Endpoint
-router.post('/login', loginUser);
-
-export default router;
